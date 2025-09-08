05f6a7470fc0386d292d5ec1fc03fd61b3c33c2a
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-09-08 00:23:28 -0500

    documentation

--- CarriganSqlTools/Carrigan.SqlTools/Extensions/TypeExtensions.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Extensions/TypeExtensions.cs
@@ -7,6 +7,15 @@ public static class TypeExtensions
 {
     private readonly static Dictionary<Type, string> TableNames = [];
     private readonly static Dictionary<Type, string> TableSchemas = [];
+
+    /// <summary>
+    /// Use reflection to get the name of the table for a given class. This is will look at the table attribution, from DataAnnotations, first.
+    /// If the annotation is not present, it will then get the name of the class.
+    /// Effectively the table name is considered to be the name of the class unless it has been overridden with the data annotation.
+    /// In the future, I would like to add a third top priority item with my own custom attribute.
+    /// </summary>
+    /// <param name="type">The data Type of the class.</param>
+    /// <returns>The assumed SQL name of the table this class represent.</returns>
     public static string TableName(this Type type)
     {
         lock (TableNames)
@@ -20,7 +29,14 @@ public static class TypeExtensions
         }
     }
 
-
+    /// <summary>
+    /// Use reflection to get the name of the schema for a given class. This is will look at the table attribution, from DataAnnotations.
+    /// If the annotation is not present, then no schema will be used in the generated SQL, 
+    /// and the query will run as the default schema for the user associated with the connection string used to connect to the database.
+    /// In the future, I would like to add a library specific top priority attribute, so users of this library don't need to use the ms data annotations.
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns>Either an empty string or a schema name</returns>
     public static string TableSchema(this Type type)
     {
         lock (TableSchemas)
--- CarriganSqlTools/Carrigan.SqlTools/Invocation/InvocationReflectorCache.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Invocation/InvocationReflectorCache.cs
@@ -4,9 +4,14 @@ using System.Reflection;
 
 namespace Carrigan.SqlTools.Invocation;
 
+/// <summary>
+/// This class is serves as a cache for reflection operations commonly used by this library.
+/// </summary>
+/// <typeparam name="T">The type that are using with reflection and want to cache.</typeparam>
 internal static class InvocationReflectorCache<T>
 {
-    internal static Type Type { get; }
+    //This library is essentially a static means of accessing lazy loaded reflection operations.
+    internal static Type Type { get; } 
     internal static Dictionary<string, PropertyInfo> Properties => _LazyProperties.Value;
 
     private static readonly Lazy<Dictionary<string, PropertyInfo>> _LazyProperties;
@@ -18,7 +23,10 @@ internal static class InvocationReflectorCache<T>
         _LazyProperties = new Lazy<Dictionary<string, PropertyInfo>>
             (() => new Dictionary<string, PropertyInfo>
                 (
-                    [.. ReflectorCache<T>
+                    //yes, we get this is caching from a lower level of caching
+                    //yes, this seems redundant, but I want to cache the reflection operations at various levels
+                    //I do this because the application I developed this library for was one of several libraries accessing the lower cache and doing different things with it.
+                    [.. ReflectorCache<T> 
                         .WriteablePublicInstanceProperties
                         .Where(property => property.GetCustomAttribute<NotMappedAttribute>() == null)
                         .Select(property => new KeyValuePair<string, PropertyInfo>(property.Name, property))]
