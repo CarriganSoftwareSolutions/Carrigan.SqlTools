61d7450b5a02bf553e1cb1308a4856ae95cb02a2
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-09-09 18:25:26 -0500

    additional comments

--- CarriganSqlTools/Carrigan.SqlTools/OffsetNexts/DefinePage.cs
+++ CarriganSqlTools/Carrigan.SqlTools/OffsetNexts/DefinePage.cs
@@ -1,8 +1,19 @@
 ﻿namespace Carrigan.SqlTools.OffsetNexts;
 
+/// <summary>
+/// This class also represents the Offset Next feature in SQL Server, 
+/// however this one wraps the concept with the more tangible idea of a page from a book.
+/// The page number and page size are used to calculate the offset and next values in a query needed to result in a given page.
+/// </summary>
 public class DefinePage : OffsetNext
 {
-    public  DefinePage(uint pageNumber, uint pageSize)
+    /// <summary>
+    /// The constructor of <see cref="DefinePage"/>
+    /// </summary>
+    /// <param name="pageNumber">Offset = (pageNumber - 1) * pageSize</param>
+    /// <param name="pageSize">Next = pageSize</param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public DefinePage(uint pageNumber, uint pageSize)
     {
         if (pageNumber <= 0 || pageSize <= 0)
             throw new ArgumentOutOfRangeException($"{nameof(pageNumber)}, {pageNumber}, and {nameof(pageSize)}, {pageSize} must have a value greater than zero.");
--- CarriganSqlTools/Carrigan.SqlTools/OffsetNexts/OffsetNext.cs
+++ CarriganSqlTools/Carrigan.SqlTools/OffsetNexts/OffsetNext.cs
@@ -1,20 +1,41 @@
 ﻿namespace Carrigan.SqlTools.OffsetNexts;
 
+/// <summary>
+/// This class represents the offset and page feature in SQL Server.  Used together this can define a page of data records.
+/// </summary>
 public class OffsetNext
 {
+    /// <summary>
+    /// Represents the SQL Offset
+    /// </summary>
     public uint Offset { get; protected set; }
+    /// <summary>
+    /// Represents the SQL Next
+    /// </summary>
     public uint Next { get; protected set; }
 
+    /// <summary>
+    /// Protected constructor needed for inheritance.
+    /// </summary>
     protected OffsetNext()
     {
     }
 
+    /// <summary>
+    /// public constructor to define Offset and Next
+    /// </summary>
+    /// <param name="offset"></param>
+    /// <param name="next"></param>
     public OffsetNext(uint offset, uint next)
     {
         Offset = offset;
         Next = next;
     }
 
+    /// <summary>
+    /// Produces the SQL for the given values Offset and Next
+    /// </summary>
+    /// <returns>the SQL for the given values Offset and Next</returns>
     public string ToSql()
     {
         if(Next == 0 && Offset ==0)
