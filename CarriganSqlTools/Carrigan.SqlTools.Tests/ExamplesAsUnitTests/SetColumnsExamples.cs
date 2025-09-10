827ac3b4938f3a2ee48d22d5deb117f32f4da23a
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-09-10 17:50:27 -0500

    Added examples for SetColumns

---
+++ CarriganSqlTools/Carrigan.SqlTools.Tests/ExamplesAsUnitTests/SetColumnsExamples.cs
@@ -0,0 +1,60 @@
+﻿using Carrigan.SqlTools.JoinTypes;
+using Carrigan.SqlTools.OffsetNexts;
+using Carrigan.SqlTools.OrderByItems;
+using Carrigan.SqlTools.Predicates;
+using Carrigan.SqlTools.Sets;
+using Carrigan.SqlTools.SqlGenerators;
+using Carrigan.SqlTools.Tests.TestEntities; //this is where Customer and Order are defined.
+using System.Text;
+
+
+namespace Carrigan.SqlTools.Tests.ExamplesAsUnitTests;
+public class SetColumnsTexamples
+{
+    private SqlGenerator<Customer> customerGenerator = new();
+    private SqlGenerator<Order> orderGenerator = new();
+
+
+    [Fact]
+    public void NoSetsColumn()
+    {
+        Customer entity = new()
+        {
+            Id = 42,
+            Name = "Hank Hill",
+            Email = "Hank.Hill@example.com",
+            Phone = "+1(555)555-5555"
+        };
+        SqlQuery query = customerGenerator.UpdateById(entity);
+
+        Assert.Equal("UPDATE [Customer] SET [Name] = @Name, [Email] = @Email, [Phone] = @Phone WHERE [Id] = @Id;", query.QueryText);
+        Assert.Equal(System.Data.CommandType.Text, query.CommandType);
+        Assert.Equal(4, query.Parameters.Count);
+
+        Assert.Equal(42, (int)query.Parameters.Where(param => param.Key == "Id").Single().Value);
+        Assert.Equal("Hank Hill", (string)query.Parameters.Where(param => param.Key == "Name").Single().Value);
+        Assert.Equal("Hank.Hill@example.com", (string)query.Parameters.Where(param => param.Key == "Email").Single().Value);
+        Assert.Equal("+1(555)555-5555", (string)query.Parameters.Where(param => param.Key == "Phone").Single().Value);
+    }
+
+    [Fact]
+    public void UsesSetsColumns()
+    {
+        //Note: SetColumns<T> validates the names of the properties, and throws an error if the property isn't valid
+        SetColumns<Customer> columns = new(nameof(Customer.Email));
+        Customer entity = new() 
+        { 
+            Id = 42, 
+            Name = "Hank", 
+            Email = "Hank@example.gov" 
+        };
+        SqlQuery query = customerGenerator.UpdateById(entity, columns);
+
+        Assert.Equal("UPDATE [Customer] SET [Email] = @Email WHERE [Id] = @Id;", query.QueryText);
+        Assert.Equal(System.Data.CommandType.Text, query.CommandType);
+        Assert.Equal(2, query.Parameters.Count);
+
+        Assert.Equal(42, (int)query.Parameters.Where(param => param.Key == "Id").Single().Value);
+        Assert.Equal("Hank@example.gov", (string)query.Parameters.Where(param => param.Key == "Email").Single().Value);
+    }
+}
--- CarriganSqlTools/Carrigan.SqlTools/Sets/SetColumns.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Sets/SetColumns.cs
@@ -1,10 +1,64 @@
 ﻿using Carrigan.Core.Extensions;
+using Carrigan.SqlTools.Predicates;
+using Carrigan.SqlTools.SqlGenerators;
 
 namespace Carrigan.SqlTools.Sets;
-
+/// <summary>
+/// This class is used as part of generating SQL updates statements. 
+/// It used when you need to change from the default behavior to update all columns to only update specific columns.
+/// This class is named to mirror the SQL "SET [Column] = @Parameter".
+/// </summary>
+/// <typeparam name="T"></typeparam>
+/// <para>Update example not using SetColumns</para>
+/// <example>
+/// <code language="csharp"><![CDATA[
+///Customer entity = new()
+///{
+///    Id = 42,
+///    Name = "Hank Hill",
+///    Email = "Hank.Hill@example.com",
+///    Phone = "+1(555)555-5555"
+///};
+///SqlQuery query = customerGenerator.UpdateById(entity);
+/// ]]></code>
+/// <para>Resulting SQL:</para>
+/// <code><![CDATA[
+/// UPDATE [Customer] SET [Name] = @Name, [Email] = @Email, [Phone] = @Phone 
+/// WHERE [Id] = @Id;
+/// ]]></code>
+/// </example>
+/// <example>
+/// <para>
+/// Update example using SetColumns
+/// Note: SetColumns&lt;T&gt; validates the names of the properties, and throws an error if the property isn't valid
+/// </para>
+/// <code language="csharp"><![CDATA[
+/// SetColumns&lt;Customer&gt; columns = new(nameof(Customer.Email));
+/// Customer entity = new()
+/// {
+///    Id = 42,
+///    Name = "Hank",
+///    Email = "Hank@example.gov"
+/// };
+/// SqlQuery query = customerGenerator.UpdateById(entity, columns);
+/// ]]></code>
+/// <para>Resulting SQL:</para>
+/// <code><![CDATA[
+/// UPDATE [Customer] SET [Email] = @Email 
+/// WHERE [Id] = @Id;
+/// ]]></code>
+/// </example>
 public class SetColumns<T>
 {
+    /// <summary>
+    /// Name of the columns used in the instance
+    /// </summary>
     public IEnumerable<string> ColumnNames { get; private set; }
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="columnNames">Names of the columns to be used in the instance</param>
+    /// <exception cref="ArgumentException"></exception>
     public SetColumns(params IEnumerable<string> columnNames)
     {
         IEnumerable<string> invalid = columnNames.Where(columnName => SqlToolsReflectorCache<T>.ColumnNamesHashSet.Contains(columnName) is false);
@@ -12,6 +66,11 @@ public class SetColumns<T>
             throw new ArgumentException($"{invalid.JoinAnd()} are not valid columns in table, {SqlToolsReflectorCache<T>.TableName}.", nameof(columnNames));
         ColumnNames = columnNames;
     }
+    /// <summary>
+    /// Add an additional column
+    /// </summary>
+    /// <param name="columnName">additional column to be add to SetColumns</param>
+    /// <exception cref="ArgumentException">Column name not found.</exception>
     public void AddColumn(string columnName)
     {
         if(SqlToolsReflectorCache<T>.ColumnNamesHashSet.Contains(columnName) is false)
