84689c93c8efb313b6bf0e70640830a04a5a89d5
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-09-13 17:40:09 -0500

    disregard previous two commits, after revisiting the code, I realized it wasnt a problem

--- CarriganSqlTools/Carrigan.SqlTools/Attributes/ProcedureAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/ProcedureAttribute.cs
@@ -5,14 +5,12 @@
 /// A data annotation to instruct Entity Framework to generate a class as a stored procedure.
 /// This should have been part of Carrigan.SqlTools
 /// </summary>
-//TODO:I think the procedure logic is flawed. I think I failed to provide a way return a result with a different data model than procedure arguments.
-//This occurred I wrote with for a procedure that had no return values, don't make that same mistake when rewriting.
 [AttributeUsage(AttributeTargets.Class)]
 [Obsolete("Do not use until I fix procedures")]
 public class ProcedureAttribute : Attribute
 {
     /// <summary>
-    /// Public getter to indicate the name  of the stored procedures.
+    /// Public getter to indicate the name of the stored procedures.
     /// </summary>
     public string Name { get; }
     public string Schema { get; }
