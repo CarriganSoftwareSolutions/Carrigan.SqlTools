2e5874864091d7d7a88296f20345865d0659043b
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-09-17 21:13:26 -0500

    Added unit tests for indetifier attributes on properties.

---
+++ CarriganSqlTools/Carrigan.SqlTools.Tests/IdentifierTests/ColumnIdentifierTests.cs
@@ -0,0 +1,25 @@
+﻿using Carrigan.SqlTools.SqlGenerators;
+using Carrigan.SqlTools.Tests.TestEntities.IdenitiferAttribute;
+
+namespace Carrigan.SqlTools.Tests.IdentifierTests;
+public class ColumnIdentifierTests
+{
+    private static readonly SqlGenerator<ColumnIdentifiers> _generator = new();
+    private static readonly ColumnIdentifiers _identifier = new ColumnIdentifiers()
+    {
+        Id = 1,
+        Property = 2,
+        ColumnName = 3,
+        IdentifierName = 4,
+        IdentifierOverrideName = 5
+    };
+
+    [Fact]
+    public void PropertyNameTest()
+    {
+        SqlQuery query = _generator.UpdateById(_identifier);
+        string actual = query.QueryText;
+        string expected = "UPDATE [ColumnIdentifiers] SET [Property] = @Property, [Column] = @Column, [Identifier] = @Identifier, [IdentifierOverride] = @IdentifierOverride WHERE [Id] = @Id;";
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ CarriganSqlTools/Carrigan.SqlTools.Tests/TestEntities/IdenitiferAttribute/ColumnIdentifiers.cs
@@ -0,0 +1,18 @@
+﻿using Carrigan.SqlTools.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Carrigan.SqlTools.Tests.TestEntities.IdenitiferAttribute;
+public class ColumnIdentifiers
+{
+    [Key]
+    public int Id { get; set; }
+    public int Property { get; set; }
+    [Column("Column")]
+    public int ColumnName { get; set; }
+    [Identifier("Identifier")]
+    public int IdentifierName { get; set; }
+    [Identifier("IdentifierOverride")]
+    [Column("Column")]
+    public int IdentifierOverrideName { get; set; }
+}
