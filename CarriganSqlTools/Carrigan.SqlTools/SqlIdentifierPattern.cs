880032fddf1814bd37c382de880d548784e525fb
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-09-21 23:02:35 -0500

    Added additional documentation to SqlIdentifierPattern

--- CarriganSqlTools/Carrigan.SqlTools/SqlIdentifierPattern.cs
+++ CarriganSqlTools/Carrigan.SqlTools/SqlIdentifierPattern.cs
@@ -9,9 +9,21 @@ namespace Carrigan.SqlTools;
 /// </summary>
 public static class SqlIdentifierPattern
 {
+    /// <summary>
+    /// Defines the pattern to check for, based on the SQL Server Identifier naming rules.
+    /// </summary>
     private static readonly string _pattern = @"^[A-Za-z_@#][A-Za-z0-9_@$#]*$";
+    /// <summary>
+    /// Determine if <see <paramref name="identifier"/> passes the test.
+    /// </summary>
+    /// <param name="identifier"></param>
+    /// <returns>true if  <see <paramref name="identifier"/> passes the test. Otherwise false</returns>
     public static bool Passes(string identifier) =>
          Regex.IsMatch(identifier, _pattern);
+    /// <summary>
+    /// Determine if <see <paramref name="identifier"/> failed the test.
+    /// </summary>
+    /// <returns>true if  <see <paramref name="identifier"/> fails the test. Otherwise false</returns>
     public static bool Fails(string identifier) =>
          Regex.IsMatch(identifier, _pattern) == false;
 }
