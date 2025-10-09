1ae87889a0a48d93f5b559ad913c7d617c5ef1ba
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-10-09 02:07:51 -0500

    code cleanup

--- CarriganSqlTools/Carrigan.SqlTools.Tests/Tags/AliasTagTests.cs
+++ CarriganSqlTools/Carrigan.SqlTools.Tests/Tags/AliasTagTests.cs
@@ -1,13 +1,7 @@
-﻿using Carrigan.Core.Extensions;
-using Carrigan.SqlTools.IdentifierTypes;
+﻿using Carrigan.SqlTools.IdentifierTypes;
 using Carrigan.SqlTools.Tags;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Carrigan.SqlTools.Tests.IdentifierTypes;
+namespace Carrigan.SqlTools.Tests.Tags;
 public class AliasTagTests
 {
     private readonly string white = " ";
