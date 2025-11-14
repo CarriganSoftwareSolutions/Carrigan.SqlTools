9b1d1d6a29678ffde3269ceb412753f0e84844da
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-14 00:47:26 -0600

    code review

---
+++ CarriganSqlTools/Carrigan.SqlTools/Exceptions/SqlTypeDoesNotSupportMaxSizeException.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Carrigan.SqlTools.Exceptions;
+public class SqlTypeDoesNotSupportMaxSizeException : Exception
+{
+    internal SqlTypeDoesNotSupportMaxSizeException(SqlDbType sqlDbType) : base
+        ($"{sqlDbType}, does not support the MAX size argument. " +
+        "MAX size is only supported by the SQL types: VarChar, NVarChar, and VarBinary ")
+    { } 
+}
--- CarriganSqlTools/Carrigan.SqlTools/Types/SqlTypeCache.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Types/SqlTypeCache.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections.ObjectModel;
 using System.Data;
+using System.Data.SqlClient;
 
 //TODO: added type mappings documentation to the readme.md file.
 
 //WHEN ADDING NEW TYPES TO THIS SELECTION,
-//be sure to add them to add types to the following places as well:
+//be sure to add them to the following places as well:
 // - Carrigan.SqlTools.ReflectorCache SqlTypeNameCache's SqlTypeNameCache Constructor
 // - Carrigan.SqlTools.IntegrationTests FieldsRoundTripTests datasets
 // - Carrigan.SqlTools.IntegrationTests.Models FieldsModel's properties.
@@ -12,7 +13,7 @@ using System.Data;
 // - Carrigan.SqlTools.Tests.InvocationTests.TypeTests Classes
 
 namespace Carrigan.SqlTools.Types;
-//TODO: Proof read documentation
+//TODO: Proof read documentation, unit tests
 /// <summary>
 /// Provides a fast, centralized mapping from CLR <see cref="Type"/> to the corresponding
 /// ADO.NET <see cref="SqlDbType"/> used by <see cref="SqlParameter.SqlDbType"/>.
@@ -28,7 +29,7 @@ namespace Carrigan.SqlTools.Types;
 ///   <item><description><see cref="decimal"/> → <see cref="SqlDbType.Decimal"/></description></item>
 /// </list>
 /// These modern mappings avoid legacy truncation and range issues such as <c>SqlDateTime overflow</c>
-/// when passing <see cref="DateTime.MinValue"/> or any other DateTime value before the 1753.
+/// when passing <see cref="DateTime.MinValue"/> or any other DateTime value before the year 1753.
 /// </para>
 /// <para>
 /// For numeric and textual types, the mapping follows SQL Server conventions:
@@ -40,41 +41,32 @@ namespace Carrigan.SqlTools.Types;
 public static class SqlTypeCache
 {
     /// <summary>
-    /// Tries to retrieve the <see cref="SqlDbType"/> for the specified CLR type.
-    /// </summary>
-    public static bool TryGetSqlDbType(Type type, out SqlDbType sqlDbType)
-    {
-        if (_cache.TryGetValue(type, out sqlDbType))
-            return true;
-        else if (type.IsEnum)
-            return _cache.TryGetValue(typeof(Enum), out sqlDbType);
-        else
-        {
-            Type underlyingType = Nullable.GetUnderlyingType(type) ?? type;
-            if (underlyingType.IsEnum)
-            {
-                return _cache.TryGetValue(typeof(Enum), out sqlDbType);
-            }
-            return false;
-        }
-    }
-
-    /// <summary>
     /// Retrieves the <see cref="SqlDbType"/> for the specified CLR type.
-    /// Throws <see cref="NotSupportedException"/> if no mapping exists.
     /// </summary>
-    public static SqlDbType GetSqlDbType(Type type)
+    internal static SqlDbType GetSqlDbType(Type type)
     {
-        if(TryGetSqlDbType(type, out SqlDbType sqlDbType))
+        Type underlyingType = Nullable.GetUnderlyingType(type) ?? type;
+        if (underlyingType.IsEnum)
+            return MapEnum(underlyingType);
+        else if (_cache.TryGetValue(underlyingType, out SqlDbType sqlDbType))
             return sqlDbType;
         else
-            throw new NotSupportedException($"No SqlDbType mapping registered for CLR type '{type.FullName}'.");
+            return SqlDbType.Variant;
     }
 
-    public static SqlDbType GetSqlDbTypeFromValue(object? value) =>
+    internal static SqlDbType GetSqlDbTypeFromValue(object? value) =>
         value is null ? SqlDbType.Variant : GetSqlDbType(value.GetType());
 
     private static readonly ReadOnlyDictionary<Type, SqlDbType> _cache;
+
+    private static SqlDbType MapEnum(Type enumType)
+    {
+        Type underlyingType = Enum.GetUnderlyingType(enumType);
+        if (_cache.TryGetValue(underlyingType, out SqlDbType sqlType)) 
+            return sqlType;
+        else
+            return SqlDbType.Variant;
+    }
 
     static SqlTypeCache()
     {
@@ -83,51 +75,36 @@ public static class SqlTypeCache
         [
             // Guid
             new(typeof(Guid),               SqlDbType.UniqueIdentifier),
-            new(typeof(Guid?),              SqlDbType.UniqueIdentifier),
 
             // Text
             new(typeof(string),             SqlDbType.NVarChar),
             new(typeof(char),               SqlDbType.NChar),
-            new(typeof(char?),              SqlDbType.NChar),
 
             //Binary
             new(typeof(byte[]),             SqlDbType.VarBinary),
 
             // Boolean
             new(typeof(bool),               SqlDbType.Bit),
-            new(typeof(bool?),              SqlDbType.Bit),
 
             // Integers
             new(typeof(byte),               SqlDbType.TinyInt),
-            new(typeof(byte?),              SqlDbType.TinyInt),
             new(typeof(sbyte),              SqlDbType.SmallInt),
-            new(typeof(sbyte?),             SqlDbType.SmallInt),
             new(typeof(short),              SqlDbType.SmallInt),
-            new(typeof(short?),             SqlDbType.SmallInt),
             new(typeof(int),                SqlDbType.Int),
-            new(typeof(int?),               SqlDbType.Int),
             new(typeof(long),               SqlDbType.BigInt),
-            new(typeof(long?),              SqlDbType.BigInt),
 
             // Decimal and floating-point
             new(typeof(float),              SqlDbType.Real),
-            new(typeof(float?),             SqlDbType.Real),
             new(typeof(double),             SqlDbType.Float),
-            new(typeof(double?),            SqlDbType.Float),
             new(typeof(decimal),            SqlDbType.Decimal),
-            new(typeof(decimal?),           SqlDbType.Decimal),
 
             // Date/Time
             new(typeof(DateTime),           SqlDbType.DateTime2),
-            new(typeof(DateTime?),          SqlDbType.DateTime2),
             new(typeof(DateOnly),           SqlDbType.Date),
-            new(typeof(DateOnly?),          SqlDbType.Date),
             new(typeof(TimeOnly),           SqlDbType.Time),
-            new(typeof(TimeOnly?),          SqlDbType.Time),
             new(typeof(DateTimeOffset),     SqlDbType.DateTimeOffset),
-            new(typeof(DateTimeOffset?),    SqlDbType.DateTimeOffset),
 
-            new(typeof(Enum),               SqlDbType.Int),
+            //Note: Is Enum is not directly handled by the cache, but rather the underlying type of the enum is handled by the cache.
 
             //TODO: XML:Not supported yet. Add in the future after issues mapping resolved, or remove.
             //new(typeof(System.Xml.Linq.XDocument), SqlDbType.Xml),
