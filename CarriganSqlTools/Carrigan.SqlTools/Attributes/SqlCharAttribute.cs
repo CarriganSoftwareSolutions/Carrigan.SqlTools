a62ed78c773e7ffddd1fd76e6805ed5cb2589a6d
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-15 22:45:16 -0600

    code review for SqlCharAttribute

--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlCharAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlCharAttribute.cs
@@ -6,22 +6,22 @@ namespace Carrigan.SqlTools.Attributes;
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 public sealed class SqlCharAttribute : SqlTypeAttribute
 {
-    public SqlCharAttribute(EncodingEnum encoding, StorageTypeEnum storageTypeEnum) : base(GetSqlDbType(encoding, storageTypeEnum))
+    public SqlCharAttribute(EncodingEnum encoding, StorageTypeEnum storageTypeEnum) : base(GetSqlTypeDefinition(encoding, storageTypeEnum))
     {
 
     }
-    public SqlCharAttribute(EncodingEnum encoding, StorageTypeEnum storageTypeEnum, int size) : base(GetSqlDbType(encoding, storageTypeEnum, size))
+    public SqlCharAttribute(EncodingEnum encoding, StorageTypeEnum storageTypeEnum, int size) : base(GetSqlTypeDefinition(encoding, storageTypeEnum, size))
     {
 
     }
 
-    private static SqlTypeDefinition GetSqlDbType(EncodingEnum encoding, StorageTypeEnum storageTypeEnum, int? size = null) => 
+    private static SqlTypeDefinition GetSqlTypeDefinition(EncodingEnum encoding, StorageTypeEnum storageTypeEnum, int? size = null) => 
         (encoding, storageTypeEnum) switch
         {
             (EncodingEnum.Ascii, StorageTypeEnum.Fixed) => SqlTypeDefinition.AsChar(size),
             (EncodingEnum.Ascii, StorageTypeEnum.Var) => SqlTypeDefinition.AsVarChar(size),
             (EncodingEnum.Unicode, StorageTypeEnum.Fixed) => SqlTypeDefinition.AsNChar(size),
             (EncodingEnum.Unicode, StorageTypeEnum.Var) => SqlTypeDefinition.AsNVarChar(size),
-            _ => throw new NotSupportedException(),
+            _ => throw new NotSupportedException($"Unsupported encoding '{encoding}' with storage type '{storageTypeEnum}'."),
         };
 }
