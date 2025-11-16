8133dcdd9acf0a0d659a9a0cc6b802f4c69f658d
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-15 22:42:18 -0600

    code review of  SqlCharAttribute

--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlCharAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlCharAttribute.cs
@@ -1,28 +1,27 @@
 ﻿using Carrigan.SqlTools.Types;
-using System.Data;
 
 namespace Carrigan.SqlTools.Attributes;
+
 //TODO: Documentation and Unit Tests
-
-[AttributeUsage(AttributeTargets.Property)]
-public class SqlCharAttribute : SqlTypeAttribute
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public sealed class SqlCharAttribute : SqlTypeAttribute
 {
-    public SqlCharAttribute(EncodingEnum encoding, StorageTypeEnum storageTypeEnum) : base (GetSqlSbType(encoding, storageTypeEnum))
+    public SqlCharAttribute(EncodingEnum encoding, StorageTypeEnum storageTypeEnum) : base(GetSqlDbType(encoding, storageTypeEnum))
     {
 
     }
-    public SqlCharAttribute(EncodingEnum encoding, StorageTypeEnum storageTypeEnum, int size) : base(GetSqlSbType(encoding, storageTypeEnum, size))
+    public SqlCharAttribute(EncodingEnum encoding, StorageTypeEnum storageTypeEnum, int size) : base(GetSqlDbType(encoding, storageTypeEnum, size))
     {
 
     }
 
-    private static SqlTypeDefinition GetSqlSbType(EncodingEnum encoding, StorageTypeEnum storageTypeEnum, int? size = null) => 
+    private static SqlTypeDefinition GetSqlDbType(EncodingEnum encoding, StorageTypeEnum storageTypeEnum, int? size = null) => 
         (encoding, storageTypeEnum) switch
         {
             (EncodingEnum.Ascii, StorageTypeEnum.Fixed) => SqlTypeDefinition.AsChar(size),
             (EncodingEnum.Ascii, StorageTypeEnum.Var) => SqlTypeDefinition.AsVarChar(size),
             (EncodingEnum.Unicode, StorageTypeEnum.Fixed) => SqlTypeDefinition.AsNChar(size),
             (EncodingEnum.Unicode, StorageTypeEnum.Var) => SqlTypeDefinition.AsNVarChar(size),
-            _ => throw new NotImplementedException(),
+            _ => throw new NotSupportedException(),
         };
 }
