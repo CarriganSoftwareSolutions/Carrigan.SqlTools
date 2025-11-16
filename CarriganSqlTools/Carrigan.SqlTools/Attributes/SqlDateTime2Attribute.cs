4f875ce31f7c059eb739a284cb9503233b14299b
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-16 00:00:32 -0600

    code review for SqlDateTime2Attribute

--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateTime2Attribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateTime2Attribute.cs
@@ -1,16 +1,14 @@
 ﻿using Carrigan.SqlTools.Types;
-using System.Data;
 
 namespace Carrigan.SqlTools.Attributes;
-//TODO: Documentation and Unit Tests
 
-[AttributeUsage(AttributeTargets.Property)]
-public class SqlDateTime2Attribute : SqlTypeAttribute
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public sealed class SqlDateTime2Attribute : SqlTypeAttribute
 {
     public SqlDateTime2Attribute() : base(SqlTypeDefinition.AsDateTime2()) 
     {
     }
-    public SqlDateTime2Attribute(byte precision) : base(SqlTypeDefinition.AsDateTime2(precision)) 
+    public SqlDateTime2Attribute(byte fractionalSecondPrecision) : base(SqlTypeDefinition.AsDateTime2(fractionalSecondPrecision)) 
     {
     }
 }
