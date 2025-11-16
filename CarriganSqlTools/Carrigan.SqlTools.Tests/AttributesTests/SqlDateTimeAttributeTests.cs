2304f4cc913bf39ba84eefdb62da93ef8e834a6b
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-16 01:01:30 -0600

    code review for SqlDateTimeAttributeTests

---
+++ CarriganSqlTools/Carrigan.SqlTools.Tests/AttributesTests/SqlDateTimeAttributeTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using Carrigan.SqlTools.Attributes;
+using Carrigan.SqlTools.Types;
+
+namespace Carrigan.SqlTools.Tests.AttributesTests;
+
+
+public sealed class SqlDateTimeAttributeTests
+{
+    [Theory]
+    [InlineData(SizeableEnum.Regular, SqlDbType.DateTime, "DATETIME")]
+    [InlineData(SizeableEnum.Smaller, SqlDbType.SmallDateTime, "SMALLDATETIME")]
+    public void Constructor_SizeableEnum(
+        SizeableEnum dateTimeSize,
+        SqlDbType expectedSqlDbType,
+        string expectedTypeDeclaration)
+    {
+        SqlDateTimeAttribute sqlDateTimeAttribute = new(dateTimeSize);
+
+        Assert.NotNull(sqlDateTimeAttribute);
+        Assert.NotNull(sqlDateTimeAttribute.SqlTypeDefinition);
+
+        SqlTypeDefinition sqlTypeDefinition = sqlDateTimeAttribute.SqlTypeDefinition;
+
+        Assert.Equal(expectedSqlDbType, sqlTypeDefinition.Type);
+        Assert.Null(sqlTypeDefinition.Size);
+        Assert.False(sqlTypeDefinition.UseMax);
+        Assert.Null(sqlTypeDefinition.Precision);
+        Assert.Null(sqlTypeDefinition.Scale);
+        Assert.Equal(expectedTypeDeclaration, sqlTypeDefinition.TypeDeclaration);
+    }
+
+    [Fact]
+    public void Constructor_SizeableEnum_Exception()
+    {
+        SizeableEnum unsupportedValue = (SizeableEnum)999;
+
+        Assert.Throws<NotSupportedException>(() => new SqlDateTimeAttribute(unsupportedValue));
+    }
+}
