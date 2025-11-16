c5838c61a1133c29bd5eb6a57a0ff8fa41864991
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-16 00:47:22 -0600

    code review for SqlDateTimeAttribute, also added initial dcoumentation for SqlDateTimeAttribute

--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateTimeAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateTimeAttribute.cs
@@ -1,20 +1,35 @@
 ﻿using Carrigan.SqlTools.Types;
-using System.Data;
 
 namespace Carrigan.SqlTools.Attributes;
-//TODO: Documentation and Unit Tests
-
-[AttributeUsage(AttributeTargets.Property)]
-public class SqlDateTimeAttribute : SqlTypeAttribute
+/// <summary>
+/// Specifies that a property represents a <c>DATETIME</c> / <c>SMALLDATETIME</c> column
+/// and overrides the default SQL type mapping for that column in the data model.
+/// </summary>
+/// <remarks>
+/// This attribute defines SQL metadata for a property that represents a <c>DATETIME</c> / <c>SMALLDATETIME</c>
+/// column on a table model, including the optional fractional-second precision.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public sealed class SqlDateTimeAttribute : SqlTypeAttribute
 {
-    public SqlDateTimeAttribute(SizeableEnum dateTimeSize) :
-        base
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SqlDateTimeAttribute"/> class and configures
+    /// the associated property to represent a <c>DATETIME</c> or <c>SMALLDATETIME</c> column.
+    /// </summary>
+    /// <param name="dateTimeSize">
+    /// Indicates whether the column uses <c>DATETIME</c> or <c>SMALLDATETIME</c>.
+    /// </param>
+    /// <exception cref="NotSupportedException">
+    /// Thrown when an unsupported <see cref="SizeableEnum"/> is provided.  
+    /// This typically indicates that the enumeration was extended without updating this method.
+    /// </exception>
+    public SqlDateTimeAttribute(SizeableEnum dateTimeSize) : base
         (
-            (dateTimeSize) switch
+            dateTimeSize switch
             {
-                (SizeableEnum.Regular) => SqlTypeDefinition.AsDateTime(),
-                (SizeableEnum.Smaller) => SqlTypeDefinition.AsSmallDateTime(),
-                _ => throw new NotImplementedException(),
+                SizeableEnum.Regular => SqlTypeDefinition.AsDateTime(),
+                SizeableEnum.Smaller => SqlTypeDefinition.AsSmallDateTime(),
+                _ => throw new NotSupportedException($"Unsupported storage type '{dateTimeSize}'."),
             }
         )
     {
