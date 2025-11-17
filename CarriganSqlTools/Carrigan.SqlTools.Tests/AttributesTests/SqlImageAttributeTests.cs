87f7bb0c640501f254c34ce0da8ccdbb316e7ef3
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-16 23:33:59 -0600

    Code Review / Unit tests for SqlImageAttributeTests

---
+++ CarriganSqlTools/Carrigan.SqlTools.Tests/AttributesTests/SqlImageAttributeTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using Carrigan.SqlTools.Attributes;
+using Carrigan.SqlTools.Types;
+
+namespace Carrigan.SqlTools.Tests.AttributesTests;
+
+public sealed class SqlImageAttributeTests
+{
+    [Fact]
+    public void Constructor()
+    {
+        SqlImageAttribute sqlImageAttribute = new();
+
+        Assert.NotNull(sqlImageAttribute);
+        Assert.NotNull(sqlImageAttribute.SqlTypeDefinition);
+
+        SqlTypeDefinition sqlTypeDefinition = sqlImageAttribute.SqlTypeDefinition;
+
+        Assert.Equal(SqlDbType.Image, sqlTypeDefinition.Type);
+        Assert.Null(sqlTypeDefinition.Size);
+        Assert.False(sqlTypeDefinition.UseMax);
+        Assert.Null(sqlTypeDefinition.Precision);
+        Assert.Null(sqlTypeDefinition.Scale);
+        Assert.Equal("IMAGE", sqlTypeDefinition.TypeDeclaration);
+    }
+}
--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlImageAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlImageAttribute.cs
@@ -1,14 +1,33 @@
 ﻿using Carrigan.SqlTools.Types;
-using System.Data;
 
 namespace Carrigan.SqlTools.Attributes;
-//TODO: Documentation and Unit Tests
 
-[AttributeUsage(AttributeTargets.Property)]
-public class SqlImageAttribute : SqlTypeAttribute
+/// <summary>
+/// Specifies that a property represents an <c>IMAGE</c> column
+/// and overrides the default SQL type mapping for that column in the data model.
+/// </summary>
+/// <remarks>
+/// This attribute defines SQL metadata for a property that represents an  
+/// <c>IMAGE</c> column on a table model.  
+///
+/// <para>
+/// The SQL Server <c>IMAGE</c> type is deprecated, but this attribute is provided to support
+/// legacy database schemas that still rely on it.  
+/// </para>
+///
+/// <para>
+/// When applied to a property, this attribute forces <see cref="Carrigan.SqlTools"/> to generate
+/// an <c>IMAGE</c> column definition regardless of the property's .NET type.
+/// </para>
+/// </remarks>
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public sealed class SqlImageAttribute : SqlTypeAttribute
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SqlImageAttribute"/> class and configures
+    /// the associated property to represent an <c>IMAGE</c> column.
+    /// </summary>
     public SqlImageAttribute() : base (SqlTypeDefinition.AsImage())
     {
-
     }
 }
