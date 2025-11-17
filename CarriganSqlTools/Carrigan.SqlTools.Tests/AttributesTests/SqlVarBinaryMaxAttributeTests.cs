c17cb1da5e8b0faf050ee162f0205cbac7e78b1d
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-17 01:22:18 -0600

    SqlVarBinaryMaxAttribute unit tests, code review, dcoumentation

---
+++ CarriganSqlTools/Carrigan.SqlTools.Tests/AttributesTests/SqlVarBinaryMaxAttributeTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using Carrigan.SqlTools.Attributes;
+using Carrigan.SqlTools.Types;
+
+namespace Carrigan.SqlTools.Tests.AttributesTests;
+
+public sealed class SqlVarBinaryMaxAttributeTests
+{
+    [Fact]
+    public void Constructor()
+    {
+        SqlVarBinaryMaxAttribute sqlVarBinaryMaxAttribute = new();
+
+        Assert.NotNull(sqlVarBinaryMaxAttribute);
+        Assert.NotNull(sqlVarBinaryMaxAttribute.SqlTypeDefinition);
+
+        SqlTypeDefinition sqlTypeDefinition = sqlVarBinaryMaxAttribute.SqlTypeDefinition;
+
+        Assert.Equal(SqlDbType.VarBinary, sqlTypeDefinition.Type);
+        Assert.Null(sqlTypeDefinition.Size);
+        Assert.True(sqlTypeDefinition.UseMax);
+        Assert.Null(sqlTypeDefinition.Precision);
+        Assert.Null(sqlTypeDefinition.Scale);
+        Assert.Equal("VARBINARY(MAX)", sqlTypeDefinition.TypeDeclaration);
+    }
+}
--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlVarBinaryMaxAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlVarBinaryMaxAttribute.cs
@@ -1,11 +1,33 @@
 ﻿using Carrigan.SqlTools.Types;
 
 namespace Carrigan.SqlTools.Attributes;
-//TODO: Documentation and Unit Tests
 
+/// <summary>
+/// Specifies that a property represents a <c>VARBINARY(MAX)</c> column
+/// and overrides the default SQL type mapping for that column in the data model.
+/// </summary>
+/// <remarks>
+/// This attribute defines SQL metadata for a property that represents a SQL Server
+/// <c>VARBINARY(MAX)</c> column on a table model.
+///
+/// <para>
+/// <c>VARBINARY(MAX)</c> is the recommended modern replacement for the legacy <c>IMAGE</c>
+/// type when modeling large binary data, such as files or blobs.
+/// </para>
+///
+/// <para><strong>Suggested C# Data Type:</strong><br/>
+/// Properties mapped to <c>VARBINARY(MAX)</c> columns should use the .NET <see cref="byte"/>[]
+/// type. ADO.NET exposes <c>VARBINARY</c> and <c>VARBINARY(MAX)</c> values as <see cref="byte"/>[]
+/// instances, making <see cref="byte"/>[] the natural representation in the data model.
+/// </para>
+/// </remarks>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 public sealed class SqlVarBinaryMaxAttribute : SqlTypeAttribute
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SqlVarBinaryMaxAttribute"/> class and configures
+    /// the associated property to represent a <c>VARBINARY(MAX)</c> column.
+    /// </summary>
     public SqlVarBinaryMaxAttribute() : base(SqlTypeDefinition.AsVarBinaryMax())
     {
     }
