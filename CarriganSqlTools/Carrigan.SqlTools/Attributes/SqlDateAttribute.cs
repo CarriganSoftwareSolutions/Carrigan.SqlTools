0e20940c2fa338e86858a3cd66ccf4e018ab6438
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-26 05:05:29 -0600

    proof read documentation in SqlDateAttribute

--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateAttribute.cs
@@ -2,18 +2,20 @@
 //TODO: proof read documentation. Unit tests.
 namespace Carrigan.SqlTools.Attributes;
 /// <summary>
-/// Specifies that a property represents a <c>DATE</c> column
-/// and overrides the default SQL type mapping for that column in the data model.
+/// Specifies that a property represents a SQL <c>DATE</c> column and overrides the
+/// default SQL type mapping for that property in the data model.
 /// </summary>
 /// <remarks>
-/// This attribute defines SQL metadata for a property that represents a <c>DATE</c> column on a table model
+/// This attribute applies SQL metadata indicating that the decorated property
+/// should be stored in a SQL Server <c>DATE</c> column, which contains only the
+/// date component (year, month, day) with no time-of-day information.
 /// </remarks>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 public sealed class SqlDateAttribute : SqlTypeAttribute
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="SqlDateTimeAttribute"/> class and configures
-    /// the associated property to represent a <c>DATE</c> column.
+    /// Initializes a new instance of the <see cref="SqlDateAttribute"/> class and configures
+    /// the associated property to be mapped to a SQL <c>DATE</c> column.
     /// </summary>
     public SqlDateAttribute() : base (SqlTypeDefinition.AsDate())
     {
