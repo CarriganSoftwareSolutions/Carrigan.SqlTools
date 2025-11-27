68b6940bc91b1b0efced7fdb3557f67ccb23f791
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-27 00:26:03 -0600

    Documentation Proof Read: SqlTypeMismatchException

--- CarriganSqlTools/Carrigan.SqlTools/Exceptions/SqlTypeMismatchException.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Exceptions/SqlTypeMismatchException.cs
@@ -5,26 +5,39 @@ using System.Data;
 using System.Reflection;
 
 namespace Carrigan.SqlTools.Exceptions;
-
+//TODO: Unit Tests
 /// <summary>
-/// This exception is thrown when the SQL  Type associated with a property uses an inherently incompatible type for the model.
+/// Represents an error that occurs when the SQL type associated with a property/value
+/// is inherently incompatible with the corresponding CLR type.
 /// </summary>
+/// <remarks>
+/// This exception is used to enforce SQL type validation with typed 
+/// compatibility rules between CLR model types and their associated SQL metadata.
+/// It is thrown only in cases of fundamental incompatibility.
+/// These Property type attributes are also enforced by a code analyzer provided with the library.
+/// </remarks>
 public sealed class SqlTypeMismatchException : Exception
 {
     /// <summary>
-    /// Represents the allowed attributes for a a given code type.
+    /// Contains the allowed <see cref="SqlTypeAttribute"/> types for a given CLR type.
     /// </summary>
     private static readonly ReadOnlyDictionary<Type, ImmutableArray<Type>> AllowedAttributes;
 
     /// <summary>
-    /// Represents the SQL types allowed to be associated with a given code type.
+    /// Contains the allowed <see cref="SqlDbType"/> values for a given CLR type.
     /// </summary>
     private static readonly ReadOnlyDictionary<Type, ImmutableArray<SqlDbType>> AllowedSqlDbTypes;
 
 
     /// <summary>
-    /// Static constructor for assigning the values to <see cref="AllowedAttributes"/> and <see cref="AllowedSqlDbTypes"/>
+    /// Initializes the static mapping tables used to validate SQL type compatibility.
     /// </summary>
+    /// <remarks>
+    /// These mappings define which SQL type attributes and SQL type enumerations are
+    /// considered valid for each CLR type. They are used by the
+    /// <see cref="Validate(PropertyInfo, SqlTypeAttribute?)"/> and
+    /// <see cref="Validate(object, SqlDbType)"/> methods.
+    /// </remarks>
     static SqlTypeMismatchException()
     {
         Dictionary<Type, ImmutableArray<Type>> attributeTypes = new
@@ -78,30 +91,38 @@ public sealed class SqlTypeMismatchException : Exception
     }
 
     /// <summary>
-    /// Private constructor that sets the error message for a sql type attribute mismatch.
+    /// Initializes a new instance of the <see cref="SqlTypeMismatchException"/> class
+    /// for a mismatch between a CLR property type and a <see cref="SqlTypeAttribute"/>.
     /// </summary>
-    /// <param name="propertyInfo">Property Info</param>
-    /// <param name="attributeMappingType"> the mismatched attribute mapped to the property</param>
+    /// <param name="propertyInfo">The property being validated.</param>
+    /// <param name="attributeMappingType">The attribute type that is incompatible with the property.</param>
     private SqlTypeMismatchException(PropertyInfo propertyInfo, Type attributeMappingType) : base
         ($"Sql Type Attribute Mismatch: Property '{propertyInfo.Name}' with C# type '{propertyInfo.PropertyType.Name}' is inherently incompatible with attribute '{attributeMappingType.Name}'.")
     { }
 
+
     /// <summary>
-    /// Private constructor that sets the error message for a <see cref="SqlDbType"/> mismatch.
+    /// Initializes a new instance of the <see cref="SqlTypeMismatchException"/> class
+    /// for a mismatch between a CLR value type and a <see cref="SqlDbType"/>.
     /// </summary>
-    /// <param name="type">the code Type</param>
-    /// <param name="attributeMappingType"> the mismatched <see cref="SqlDbType"/></param>
+    /// <param name="type">The CLR type that is incompatible.</param>
+    /// <param name="sqlDbType">The corresponding SQL type that caused the mismatch.</param>
     private SqlTypeMismatchException(Type type, SqlDbType sqlDbType) : base
         ($"Sql Type Attribute Mismatch: C# type '{type.Name}' is inherently incompatible with SQL type '{sqlDbType}'.")
 
     { }
 
+
     /// <summary>
-    /// static method uses to test a property and its associated attribute that inherits from <see cref="SqlTypeAttribute"/>
+    /// Validates whether a CLR property and its associated <see cref="SqlTypeAttribute"/>
+    /// represent a compatible type pairing.
     /// </summary>
-    /// <param name="propertyInfo">Property Info</param>
-    /// <param name="attributeMappingType"> the attribute mapped to the property</param>
-    /// <returns>returns null if there is no mismatch, returns an exception if there is a type mismatch.</returns>
+    /// <param name="propertyInfo">The property to validate.</param>
+    /// <param name="sqlTypeAttribute">The SQL type attribute applied to the property.</param>
+    /// <returns>
+    /// <c>null</c> if the types are compatible, or an instance of
+    /// <see cref="SqlTypeMismatchException"/> if the types are inherently incompatible.
+    /// </returns>
     public static SqlTypeMismatchException? Validate(PropertyInfo propertyInfo, SqlTypeAttribute? sqlTypeAttribute)
     {
         static bool TestTheType(Type propertyType, Type attributeMappingType)
@@ -126,14 +147,18 @@ public sealed class SqlTypeMismatchException : Exception
     }
 
     /// <summary>
-    /// static method uses to test a a value and its associated <see cref="SqlDbType"/>.
+    /// Validates whether a CLR value is compatible with a specific <see cref="SqlDbType"/>.
     /// </summary>
     /// <remarks>
-    ///This is used for validating parameter values with an <see cref="SqlDbType"/>.
+    /// This method is typically used when validating parameter values assigned to
+    /// ADO.NET commands.
     /// </remarks>
-    /// <param name="value">a value</param>
-    /// <param name="sqlDbType">a <see cref="SqlDbType"/></param>
-    /// <returns></returns>
+    /// <param name="value">The CLR value to validate.</param>
+    /// <param name="sqlDbType">The SQL type that the value is being assigned to.</param>
+    /// <returns>
+    /// <c>null</c> if the value is compatible with the specified SQL type, or an instance of
+    /// <see cref="SqlTypeMismatchException"/> if a mismatch is detected.
+    /// </returns>
     public static SqlTypeMismatchException? Validate(object value, SqlDbType sqlDbType)
     {
         static bool TestTheType(Type propertyType, SqlDbType sqlDbType)
