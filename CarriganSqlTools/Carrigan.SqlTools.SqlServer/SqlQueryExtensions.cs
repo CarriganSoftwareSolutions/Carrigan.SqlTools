4c3923ea7f1613129dbcfc62feea14eea9cb789d
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-11-30 22:55:31 -0600

    proof read documentation

--- CarriganSqlTools/Carrigan.SqlTools.SqlServer/SqlQueryExtensions.cs
+++ CarriganSqlTools/Carrigan.SqlTools.SqlServer/SqlQueryExtensions.cs
@@ -7,10 +7,42 @@ namespace Carrigan.SqlTools.SqlServer;
 internal static class SqlQueryExtensions
 {
     /// <summary>
-    /// Converts parameters in an <see cref="SqlQuery"/> to <see cref="IEnumerable{T}"/> of <see cref="SqlParameter"/>
+    /// Converts the parameters in an <see cref="SqlQuery"/> into an
+    /// <see cref="IEnumerable{SqlParameter}"/> for consumption by
+    /// <see cref="Commands"/> or <see cref="CommandsAsync"/>.
     /// </summary>
-    /// <param name="query">an SqlQuery</param>
-    /// <returns><see cref="IEnumerable{T}"/> of <see cref="SqlParameter"/> for easy consumption by <see cref="Commands"/> or  <see cref="CommandsAsync"/> </returns>
+    /// <param name="query">The <see cref="SqlQuery"/> containing parameter metadata and values.</param>
+    /// <returns>
+    /// An <see cref="IEnumerable{SqlParameter}"/> mapping each <see cref="ParameterTag"/>
+    /// in the query to a fully populated <see cref="SqlParameter"/> instance.
+    /// </returns>
+    /// <remarks>
+    /// <para>
+    /// For each parameter, this method:
+    /// </para>
+    /// <list type="bullet">
+    ///     <item>
+    ///         <description>
+    ///         Creates a <see cref="SqlParameter"/> using the parameter name and value.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <description>
+    ///         Determines the correct SqlDbType using the associated
+    ///         <see cref="SqlTypeDefinition"/> if present, or derives one automatically
+    ///         via <see cref="SqlTypeDefinition"/> if no explicit SQL type was provided.
+    ///         </description>
+    ///     </item>
+    ///     <item>
+    ///         <description>
+    ///         Applies size, precision, scale, or <c>MAX</c> semantics based on the
+    ///         <see cref="SqlTypeDefinition"/> assigned to the corresponding
+    ///         <see cref="ParameterTag"/>.
+    ///         </description>
+    ///     </item>
+    /// </list>
+    /// </remarks>
+
     internal static IEnumerable<SqlParameter> GetParameterCollection(this SqlQuery query)
     {
         static SqlParameter GetSqlParameter(ParameterTag parameter, object value)
