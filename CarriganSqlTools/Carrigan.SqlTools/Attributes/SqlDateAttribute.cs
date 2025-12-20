17732b30fd16e0d5ddb58ed60e70245487a05e24
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-12-19 22:54:50 -0600

    code review

--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateAttribute.cs
@@ -1,14 +1,23 @@
 ﻿using Carrigan.SqlTools.Types;
 
 namespace Carrigan.SqlTools.Attributes;
+
 /// <summary>
 /// Specifies that a property represents a SQL <c>DATE</c> column and overrides the
 /// default SQL type mapping for that property in the data model.
 /// </summary>
 /// <remarks>
-/// This attribute applies SQL metadata indicating that the decorated property
-/// should be stored in a SQL Server <c>DATE</c> column, which contains only the
-/// date component (year, month, day) with no time-of-day information.
+/// <para>
+/// In Carrigan.SqlTools, this attribute supplies the <see cref="SqlTypeDefinition"/> used by the SQL generator
+/// when emitting SQL for the decorated property.
+/// </para>
+/// <para>
+/// SQL Server <c>DATE</c> stores only the date component (year, month, day) with no time-of-day information.
+/// </para>
+/// <para>
+/// This attribute affects only SQL generation within <c>Carrigan.SqlTools</c> and does not influence Entity Framework
+/// or database schema.
+/// </para>
 /// </remarks>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 public sealed class SqlDateAttribute : SqlTypeAttribute
