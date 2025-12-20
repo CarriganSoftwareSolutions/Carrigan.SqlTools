3daf95d784d3f7546bf30402291345ae51d45cc9
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-12-19 23:16:58 -0600

    code review

--- CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateTimeAttribute.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Attributes/SqlDateTimeAttribute.cs
@@ -1,41 +1,43 @@
 ﻿using Carrigan.SqlTools.Types;
 
 namespace Carrigan.SqlTools.Attributes;
+
 /// <summary>
-/// Specifies that a property represents a <c>DATETIME</c> or <c>SMALLDATETIME</c> column
-/// and overrides the default SQL type mapping for that column in the data model.
+/// Specifies that a property represents a SQL <c>DATETIME</c> or <c>SMALLDATETIME</c> column and overrides
+/// the default SQL type mapping for that column in the data model.
 /// </summary>
 /// <remarks>
-/// This attribute defines SQL metadata for a property that represents a <c>DATETIME</c>
-/// or <c>SMALLDATETIME</c> column on a table model.  
-/// The selected SQL type depends on the <see cref="SizeableEnum"/> value supplied.
+/// This attribute defines SQL metadata for a property, and that property represents an SQL column in the data model.
+/// The SQL type is selected by the <see cref="SizeableEnum"/> value supplied:
+/// <list type="bullet">
+/// <item><description><see cref="SizeableEnum.Regular"/> produces <c>DATETIME</c>.</description></item>
+/// <item><description><see cref="SizeableEnum.Smaller"/> produces <c>SMALLDATETIME</c>.</description></item>
+/// </list>
+/// This attribute affects only SQL generation within <c>Carrigan.SqlTools</c> and does not influence Entity Framework
+/// or database schema.
 /// </remarks>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
 public sealed class SqlDateTimeAttribute : SqlTypeAttribute
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="SqlDateTimeAttribute"/> class and configures
-    /// the associated property to represent a <c>DATETIME</c> or <c>SMALLDATETIME</c> column,
-    /// based on the specified <see cref="SizeableEnum"/>.
+    /// Initializes a new instance of the <see cref="SqlDateTimeAttribute"/> class and configures the associated
+    /// property to represent a <c>DATETIME</c> or <c>SMALLDATETIME</c> column, based on <paramref name="sizeableEnum"/>.
     /// </summary>
-    /// <param name="dateTimeSize">
-    /// Determines whether the column is generated as <c>DATETIME</c> 
-    /// (<see cref="SizeableEnum.Regular"/>) or <c>SMALLDATETIME</c> 
-    /// (<see cref="SizeableEnum.Smaller"/>).
-    /// </param>
+    /// <param name="sizeableEnum">Selects whether the SQL type is <c>DATETIME</c> or <c>SMALLDATETIME</c>.</param>
     /// <exception cref="NotSupportedException">
-    /// Thrown when an unsupported <see cref="SizeableEnum"/> value is provided.
-    /// This typically indicates that the enumeration was extended without updating the attribute.
+    /// Thrown when an unsupported <see cref="SizeableEnum"/> value is supplied. This typically indicates that the
+    /// enumeration was extended without updating the attribute.
     /// </exception>
-    public SqlDateTimeAttribute(SizeableEnum dateTimeSize) : base
-        (
-            dateTimeSize switch
-            {
-                SizeableEnum.Regular => SqlTypeDefinition.AsDateTime(),
-                SizeableEnum.Smaller => SqlTypeDefinition.AsSmallDateTime(),
-                _ => throw new NotSupportedException($"Unsupported storage type '{dateTimeSize}'."),
-            }
-        )
+    public SqlDateTimeAttribute(SizeableEnum sizeableEnum)
+        : base(GetSqlTypeDefinition(sizeableEnum))
     {
     }
+
+    private static SqlTypeDefinition GetSqlTypeDefinition(SizeableEnum sizeableEnum) =>
+        sizeableEnum switch
+        {
+            SizeableEnum.Regular => SqlTypeDefinition.AsDateTime(),
+            SizeableEnum.Smaller => SqlTypeDefinition.AsSmallDateTime(),
+            _ => throw new NotSupportedException($"Unsupported date/time size '{sizeableEnum}'."),
+        };
 }
