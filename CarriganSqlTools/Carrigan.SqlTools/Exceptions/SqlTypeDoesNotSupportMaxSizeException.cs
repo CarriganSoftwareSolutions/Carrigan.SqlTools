5658ed320f08f5b360f6747ee40459c78c5d891a
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-12-20 18:07:19 -0600

    code review

--- CarriganSqlTools/Carrigan.SqlTools/Exceptions/SqlTypeDoesNotSupportMaxSizeException.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Exceptions/SqlTypeDoesNotSupportMaxSizeException.cs
@@ -1,10 +1,22 @@
 ﻿using System.Data;
 
 namespace Carrigan.SqlTools.Exceptions;
+
+/// <summary>
+/// Thrown when a SQL type is configured with <c>MAX</c> sizing, but the specified
+/// <see cref="SqlDbType"/> does not support <c>MAX</c>.
+/// </summary>
+/// <remarks>
+/// In SQL Server, <c>MAX</c> sizing is supported only by a limited set of types
+/// (for example: <c>VARCHAR(MAX)</c>, <c>NVARCHAR(MAX)</c>, <c>VARBINARY(MAX)</c>).
+/// </remarks>
 public class SqlTypeDoesNotSupportMaxSizeException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SqlTypeDoesNotSupportMaxSizeException"/> class.
+    /// </summary>
+    /// <param name="sqlDbType">The SQL type that was incorrectly configured to use <c>MAX</c> sizing.</param>
     internal SqlTypeDoesNotSupportMaxSizeException(SqlDbType sqlDbType) : base
-        ($"{sqlDbType}, does not support the MAX size argument. " +
-        "MAX size is only supported by the SQL types: VarChar, NVarChar, and VarBinary ")
+        ($"{sqlDbType} does not support the MAX size argument. MAX size is supported only by VarChar, NVarChar, and VarBinary.")
     { } 
 }
