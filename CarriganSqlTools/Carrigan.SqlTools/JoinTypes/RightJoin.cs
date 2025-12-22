ff85ddf9b5fd3477caab0d45c4dabb0d613bf660
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2025-12-22 03:16:55 -0600

    RightJoin initial check in

---
+++ CarriganSqlTools/Carrigan.SqlTools/JoinTypes/RightJoin.cs
@@ -0,0 +1,100 @@
+﻿using Carrigan.SqlTools.PredicatesLogic;
+using Carrigan.SqlTools.ReflectorCache;
+using Carrigan.SqlTools.Tags;
+
+namespace Carrigan.SqlTools.JoinTypes;
+
+/// <summary>
+/// Represents an SQL <c>RIGHT JOIN</c> operation.
+/// </summary>
+/// <typeparam name="rightT">
+/// The data model representing the right-side table being joined.
+/// </typeparam>
+/// <example>
+/// <para>
+/// Note: <see cref="ColumnEqualsColumn{leftT, rightT}"/> validates property names and throws an exception if a property name is invalid.
+/// </para>
+/// <code language="csharp"><![CDATA[
+/// ColumnEqualsColumn<Customer, Order> predicate = new(nameof(Customer.Id), nameof(Order.CustomerId));
+/// JoinsBase join = Joins<Customer>.RightJoin<Order>(predicate);
+///
+/// SqlQuery query = customerGenerator.Select(null, join, null, null, null);
+/// ]]></code>
+/// <para>Resulting SQL:</para>
+/// <code><![CDATA[
+/// SELECT [Customer].*
+/// FROM [Customer]
+/// RIGHT JOIN [Order]
+/// ON ([Customer].[Id] = [Order].[CustomerId])
+/// ]]></code>
+/// </example>
+public class RightJoin<rightT> : JoinBase
+{
+    private readonly string _sql;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RightJoin{rightT}"/> class.
+    /// </summary>
+    /// <param name="predicates">
+    /// The predicate(s) that define the <c>ON</c> clause of the SQL <c>RIGHT JOIN</c>.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="predicates"/> is <c>null</c>.
+    /// </exception>
+    /// <exception cref="TypeInitializationException">
+    /// Thrown when the SQL reflection cache for <typeparamref name="rightT"/> fails to initialize.
+    /// </exception>
+    public RightJoin(Predicates predicates)
+        : base(predicates ?? throw new ArgumentNullException(nameof(predicates))) =>
+        _sql = $"RIGHT JOIN {TableTag} ON {_predicates.ToSql()}";
+
+    /// <summary>
+    /// Creates and returns a new <see cref="Joins{leftT}"/> object that contains
+    /// a newly created <see cref="RightJoin{rightT}"/> operation.
+    /// </summary>
+    /// <typeparam name="leftT">
+    /// The data model representing the right (base) table being joined onto.
+    /// </typeparam>
+    /// <param name="predicate">
+    /// The predicate(s) that define the <c>ON</c> clause of the SQL <c>RIGHT JOIN</c>.
+    /// </param>
+    /// <returns>
+    /// A new <see cref="Joins{leftT}"/> object containing a single <see cref="RightJoin{rightT}"/> instance.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown when <paramref name="predicate"/> is <c>null</c>.
+    /// </exception>
+    public static Joins<leftT> Joins<leftT>(Predicates predicate) =>
+        new(new RightJoin<rightT>(predicate));
+
+    /// <summary>
+    /// Creates and returns a new <see cref="Joins{leftT}"/> object that contains
+    /// the current <see cref="RightJoin{rightT}"/> instance.
+    /// </summary>
+    /// <typeparam name="leftT">
+    /// The data model representing the right (base) table being joined onto.
+    /// </typeparam>
+    /// <returns>
+    /// A new <see cref="Joins{leftT}"/> object containing the current <see cref="RightJoin{rightT}"/> instance.
+    /// </returns>
+    public Joins<leftT> AsJoins<leftT>() =>
+        new(this);
+
+    /// <summary>
+    /// Gets the <see cref="TableTag"/> associated with the right-side table in the <c>Right JOIN</c> operation.
+    /// </summary>
+    /// <exception cref="TypeInitializationException">
+    /// Thrown when the SQL reflection cache for <typeparamref name="rightT"/> fails to initialize.
+    /// </exception>
+    internal override TableTag TableTag =>
+        SqlToolsReflectorCache<rightT>.Table;
+
+    /// <summary>
+    /// Converts the current <see cref="RightJoin{rightT}"/> instance to its SQL representation.
+    /// </summary>
+    /// <returns>
+    /// A SQL string representing the <c>Right JOIN</c> clause.
+    /// </returns>
+    internal override string ToSql() =>
+        _sql;
+}
