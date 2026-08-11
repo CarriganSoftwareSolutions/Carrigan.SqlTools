fadd4bff41bc119d33a893711636e2d0f4128279
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2026-08-11 14:29:03 -0500

    changed type name for clarity

--- CarriganSqlTools/Carrigan.SqlTools.Generators.SqlServer/Expressions/Parameter.cs
+++ CarriganSqlTools/Carrigan.SqlTools.Generators.SqlServer/Expressions/Parameter.cs
@@ -8,7 +8,7 @@ namespace Carrigan.SqlTools.Expressions;
 /// <summary>
 /// Represents a SQL parameter and its corresponding value as a leaf node within a SQL expression tree.
 /// </summary>
-/// <typeparam name="T">The model type whose C# properties supply parameter metadata.</typeparam>
+/// <typeparam name="modelT">The model type whose C# properties supply parameter metadata.</typeparam>
 /// <example>
 /// <code language="csharp"><![CDATA[
 /// Parameter<Customer> parameterName = new(nameof(Customer.Name), "Hank");
@@ -28,14 +28,14 @@ namespace Carrigan.SqlTools.Expressions;
 /// WHERE ([Customer].[Name] = @Name_1)
 /// ]]></code>
 /// </example>
-public class Parameter<T> : Parameter where T : class
+public class Parameter<modelT> : Parameter where modelT : class
 {
     /// <summary>
     /// Initializes a new instance of <see cref="Parameter"/> that resolves parameter metadata from the specified model property and uses the provided value.
     /// </summary>
     /// <param name="propertyName">The property name that identifies the model property represented by the parameter.</param>
     /// <param name="value">Value to assign to the parameter; may be null.</param>
-    public Parameter(PropertyName propertyName, object? value) : base(value, SqlToolsReflectorCache<T>.GetColumnsFromProperty(DialectStatics.SupportedTypes, propertyName))
+    public Parameter(PropertyName propertyName, object? value) : base(value, SqlToolsReflectorCache<modelT>.GetColumnsFromProperty(DialectStatics.SupportedTypes, propertyName))
     {
     }
     /// <summary>
