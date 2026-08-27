ff90d0a280ad9baf3ee8c5d9c9d72dcbe4d7d5c3
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2026-08-27 03:29:32 -0500

    the PostgreSql versions of the test from the last commit.,

---
+++ CarriganSqlTools/Carrigan.SqlTools.Generators.PostgreSql.Tests/ExpressionsTests/NumericColumnBaseTests/NullableTestEntityColumnBaseTest.cs
@@ -0,0 +1,48 @@
+﻿using Carrigan.SqlTools.Base.Tests.TestEntities;
+using Carrigan.SqlTools.Expressions;
+using Carrigan.SqlTools.IdentifierTypes;
+
+namespace Carrigan.SqlTools.Generators.PostgreSql.Tests.ExpressionsTests.NumericColumnBaseTests;
+
+public class NullableTestEntityColumnBaseTest : SqlServerNumericColumnBaseTest<NullableTestEntity>
+{
+    protected override string? SchemaName =>
+        null;
+
+    protected override string TableName =>
+        "NullableTestEntity";
+
+    protected override IEnumerable<string> NumericProperties =>
+        [
+            nameof(NullableTestEntity.IntValue),
+            nameof(NullableTestEntity.LongValue),
+            nameof(NullableTestEntity.ShortValue),
+            nameof(NullableTestEntity.ByteValue),
+            nameof(NullableTestEntity.DecimalValue),
+            nameof(NullableTestEntity.FloatValue),
+            nameof(NullableTestEntity.DoubleValue)
+        ];
+
+    internal override Dictionary<string, ColumnName> ExpectedPropertyColumnName =>
+    new
+    (
+        [
+            NewKvp(nameof(NullableTestEntity.Key)),
+            NewKvp(nameof(NullableTestEntity.IntValue)),
+            NewKvp(nameof(NullableTestEntity.LongValue)),
+            NewKvp(nameof(NullableTestEntity.ShortValue)),
+            NewKvp(nameof(NullableTestEntity.ByteValue)),
+            NewKvp(nameof(NullableTestEntity.BoolValue)),
+            NewKvp(nameof(NullableTestEntity.DecimalValue)),
+            NewKvp(nameof(NullableTestEntity.FloatValue)),
+            NewKvp(nameof(NullableTestEntity.DoubleValue)),
+            NewKvp(nameof(NullableTestEntity.DateTimeValue)),
+            NewKvp(nameof(NullableTestEntity.GuidValue)),
+            NewKvp(nameof(NullableTestEntity.CharValue)),
+            NewKvp(nameof(NullableTestEntity.TimeOnlyValue)),
+            NewKvp(nameof(NullableTestEntity.DateOnlyValue)),
+            NewKvp(nameof(NullableTestEntity.ByteArrayValue)),
+            NewKvp(nameof(NullableTestEntity.DateTimeOffsetValue)),
+        ]
+    );
+}
---
+++ CarriganSqlTools/Carrigan.SqlTools.Generators.PostgreSql.Tests/ExpressionsTests/NumericColumnBaseTests/SqlServerColumnBaseTest.cs
@@ -0,0 +1,64 @@
+﻿using Carrigan.SqlTools.Base.Tests.Expressions;
+using Carrigan.SqlTools.Dialects;
+using Carrigan.SqlTools.Expressions;
+using Carrigan.SqlTools.IdentifierTypes;
+using Carrigan.SqlTools.PredicatesLogic;
+
+namespace Carrigan.SqlTools.Generators.PostgreSql.Tests.ExpressionsTests.NumericColumnBaseTests;
+
+public abstract class SqlServerNumericColumnBaseTest<modelT> : NumericColumnBaseTests<modelT> where modelT : class
+{
+    protected override ISqlDialects Dialect =>
+        new PostgreSqlDialect();
+
+    protected override string ExpectSqlFragment(string expectedColumnName) =>
+        SchemaName is null
+            ? string.Format("\"{0}\".\"{1}\"", TableName, expectedColumnName)
+            : string.Format("\"{0}\".\"{1}\".\"{2}\"", SchemaName, TableName, expectedColumnName);
+
+    protected override NumericColumn<modelT> NewNumericColumn(string propertyName) =>
+        new(propertyName);
+    protected override NumericColumn<modelT> NewNumericColumn(PropertyName propertyName) =>
+        new(propertyName);
+
+
+
+    protected override NumericExpression NewColumnAsNumericExpression(string propertyName)
+    {
+        Column<modelT> column = new(propertyName);
+        NumericExpression numericExpression = column;
+        return numericExpression;
+    }
+    protected override NumericExpression NewColumnAsNumericExpression(PropertyName propertyName)
+    {
+        Column<modelT> column = new(propertyName);
+        NumericExpression numericExpression = column;
+        return numericExpression;
+    }
+
+    protected override NumericColumnBase<modelT> NewColumnAsNumericColumnBase(string propertyName)
+    {
+        Column<modelT> column = new(propertyName);
+        NumericColumnBase<modelT> numericExpression = column;
+        return numericExpression;
+    }
+    protected override NumericColumnBase<modelT> NewColumnAsNumericColumnBase(PropertyName propertyName)
+    {
+        Column<modelT> column = new(propertyName);
+        NumericColumnBase<modelT> numericExpression = column;
+        return numericExpression;
+    }
+
+    protected override NumericColumnBase<modelT> NewColumnAsNumericColumn(string propertyName)
+    {
+        Column<modelT> column = new(propertyName);
+        NumericColumn<modelT> numericExpression = column;
+        return numericExpression;
+    }
+    protected override NumericColumnBase<modelT> NewColumnAsNumericColumn(PropertyName propertyName)
+    {
+        Column<modelT> column = new(propertyName);
+        NumericColumn<modelT> numericExpression = column;
+        return numericExpression;
+    }
+}
