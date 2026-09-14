b31484ead3dd12e4984b23e5de2a6113d745be9d
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2026-09-14 05:51:10 -0500

    coalesce comments

--- CarriganSqlTools/Carrigan.SqlTools/Expressions/Coalesce.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Expressions/Coalesce.cs
@@ -6,12 +6,27 @@ using Carrigan.Core.Extensions;
 
 namespace Carrigan.SqlTools.Expressions;
 
+/// <summary>
+/// Represents a SQL COALESCE expression that returns the first non-null value from a list of expressions.
+/// </summary>
 public class Coalesce : SqlExpression
 {
+    /// <summary>
+    /// Gets the list of values to evaluate in the COALESCE expression.
+    /// </summary>
     private IEnumerable<SqlExpression> Values { get; init; }
+
+    /// <summary>
+    /// Gets the leaf tables involved in the COALESCE expression.
+    /// </summary>
     public override IEnumerable<TableTag> LeafTables { get; }
 
-    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Coalesce"/> class with the specified values.
+    /// </summary>
+    /// <param name="values">
+    /// An array of <see cref="SqlExpression"/> instances representing the values to evaluate in the COALESCE expression. Must contain at least two values.
+    /// </param>
     public Coalesce(params IEnumerable<SqlExpression> values) : base([])
     {
         IEnumerable<TableTag> GetLeafTables()
@@ -25,6 +40,18 @@ public class Coalesce : SqlExpression
         LeafTables = GetLeafTables();
     }
 
+    /// <summary>
+    /// Validates the provided values for the COALESCE expression, ensuring that there are at least two values.
+    /// </summary>
+    /// <param name="values">
+    /// An enumerable of <see cref="SqlExpression"/> instances to validate.
+    /// </param>
+    /// <returns>
+    /// The validated enumerable of <see cref="SqlExpression"/> instances.
+    /// </returns>
+    /// <exception cref="ArgumentException">
+    /// Thrown if the number of values is less than two.
+    /// </exception>
     private static IEnumerable<SqlExpression> ValidateValues(IEnumerable<SqlExpression> values)
     {
         ArgumentNullException.ThrowIfNull(values);
@@ -35,6 +62,15 @@ public class Coalesce : SqlExpression
         return values;
     }
 
+    /// <summary>
+    /// Converts the COALESCE expression into a sequence of SQL fragments based on the specified SQL dialect.
+    /// </summary>
+    /// <param name="dialect">
+    /// The SQL dialect to use for rendering the SQL fragments.
+    /// </param>
+    /// <returns>
+    /// An enumerable of <see cref="ISqlFragment"/> instances representing the SQL fragments of the COALESCE expression.
+    /// </returns>
     public override IEnumerable<ISqlFragment> ToSqlFragments(ISqlDialects dialect)
     {
         yield return new SqlFragmentText("COALESCE(");
@@ -45,6 +81,16 @@ public class Coalesce : SqlExpression
         yield return ISqlFragment.CloseParentheses;
     }
 
+    /// <summary>
+    /// Determines whether the COALESCE expression is an aggregate expression based on the aggregate status of its values.
+    /// </summary>
+    /// <returns>
+    /// A boolean indicating whether the COALESCE expression is an aggregate expression. Returns true if all values are aggregate expressions; otherwise, false. 
+    /// If the aggregate status of the values is inconsistent, an <see cref="AggregateInconsistencyException"/> is thrown.
+    /// </returns>
+    /// <exception cref="AggregateInconsistencyException">
+    /// Thrown if the aggregate status of the values is inconsistent (i.e., some values are aggregate expressions while others are not).
+    /// </exception>
     public override bool IsAggregate()
     {
         if(Values.Select(value => value.IsAggregate()).AllEqual() ?? false)
