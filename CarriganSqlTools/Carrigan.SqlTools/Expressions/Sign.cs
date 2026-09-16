9caa04b9344a584d8e7091be1d10b784c4946198
Jonathan Carrigan <228126051+JonathanCarrigan@users.noreply.github.com>
2026-09-16 05:14:21 -0500

    Sign documentation

--- CarriganSqlTools/Carrigan.SqlTools/Expressions/Sign.cs
+++ CarriganSqlTools/Carrigan.SqlTools/Expressions/Sign.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Carrigan.Core.Interfaces.IModels;
+using Carrigan.SqlTools.SqlGenerators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +14,22 @@ namespace Carrigan.SqlTools.Expressions;
 /// </summary>
 /// <example>
 /// <code language="csharp"><![CDATA[
-///
+/// Sign expression = new(new Column<Order>(nameof(Order.Total)));
+/// SelectTags selects = new(new SelectTag(expression, "Value"));
+/// 
+/// SelectBuilder<Order> selectBuilder = new()
+/// {
+///     Selects = selects
+/// };
+/// 
+/// SqlQuery query = orderGenerator.Select(selectBuilder);
 /// ]]></code>
 /// <para>Resulting SQL:</para>
 /// <code><![CDATA[
 /// --SqlServer
-/// 
+/// SELECT SIGN([Order].[Total]) AS [Value] FROM [Order]
 /// --PostgreSql
-/// 
+/// SELECT SIGN(\"Order\".\"Total\") AS \"Value\" FROM \"Order\"
 /// ]]></code>
 /// </example>
 public class Sign : FunctionalExpression
